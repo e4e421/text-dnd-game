558e8d775b64b5505be81fc404134a56ce196974
e4e421 <e44hsaqu@duck.com>
2025-08-20 14:18:02 +0800

    fix bug of function level variable conflicting
    the function level variable of "player_name" is not passed to global scope so can not be used by storylines, fix the bug

--- text-dnd.py
+++ text-dnd.py
@@ -34,9 +34,9 @@ def encounter(npc):
             print(f"An angle have blessed you with health and luck.")
 
 def get_player_status():
-    print(f"player {player_name} status as: \n ")
-    print(f"health: {player_health}\n")
-    print(f"level: {player_level}\n")
+    print(f"player {player_name} status as: ")
+    print(f"health: {player_health}")
+    print(f"level: {player_level}")
     return None
 
 def move(input):
@@ -66,15 +66,13 @@ def assign_new_rank():
 
 #activate storyline
 
-def activate_storyline():
+def set_player_name():
     player_name = input("Welcome to the world of ESG, I am the lord here." \
     " how my I call you my brave worrier? \n")
-    if input(f"Awesome, welcome to this world my friend. From your record," \
-    "you are {player_name} right?(y/n)") == "y":
+    if input(f"Awesome, welcome to this world my friend. From your record, you are {player_name} right?(y/n)") == "y":
         print(f"Welcome to the world {player_name}, as you are new here, I would like to entroduce you to the 'ranking' system.")
-        activate_chapter_1()
     else:
-        player_name = input("sorry I didn't catch that, how my I call you?\n")
+        player_name = input("sorry I didn't catch that, how my I call you?")
     return player_name
 
 def activate_chapter_1():
@@ -98,7 +96,7 @@ key = ""
 
 while key != "quit":
     key = input("command line")
-    player_name = activate_storyline()
+    player_name = set_player_name()
     if key == "player_status":
         get_player_status()
     elif key == "w" or key == "a" or key == "s" or key == "d":
